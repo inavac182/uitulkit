109233ad10df1e96d4b0f270f989ffdb1513d338
Felipe Nava <felipe.nava.co@gmail.com>
2020-06-02 19:21:53 -0700

    feat(chore): add list of accepted commit subjets

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,5 +2,17 @@ module.exports = {
   extends: 
   [
       '@commitlint/config-conventional'
-  ]
+  ],
+  rules: {
+    'scope-empty': [2, 'never'],
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'button',
+        'card',
+        'chore'
+      ]
+    ]
+  }
 }
