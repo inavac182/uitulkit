374f8012238f948ffc47f0cfc499962e28201e64
Felipe Nava <felipe.nava.co@gmail.com>
2020-06-07 00:29:42 -0700

    chore(misc): fix tsconfig for src

--- packages/demo/src/index.tsx
+++ packages/demo/src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.css';
 import { App } from './components';
 import * as serviceWorker from './serviceWorker';
 
