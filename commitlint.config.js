e6a4919319b0349fa5eb937a56cbfa0ff9a7961a
Felipe Nava <felipe.nava.co@gmail.com>
2020-06-06 18:57:53 -0700

    chore(misc): configured commitlint removed yarn lock

---
+++ commitlint.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: 
+  [
+      '@commitlint/config-conventional'
+  ],
+  rules: {
+    'scope-empty': [2, 'never'],
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'button',
+        'card',
+        'misc',
+        'release',
+      ]
+    ]
+  }
+}
